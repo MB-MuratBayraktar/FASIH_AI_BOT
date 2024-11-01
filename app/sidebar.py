04a4ef60a65779af1dae2b7280bc338bc4295483
murat <mr.muratbayraktar@gmail.com>
2024-11-01 22:15:36 +0100

    added fastapi service from render

--- app/sidebar.py
+++ app/sidebar.py
@@ -10,20 +10,29 @@ def display_sidebar():
     # Display the image in the sidebar
     st.sidebar.image(str(image_path), use_column_width=True)
 
-    model_options = ["gpt-3.5-turbo-0125", "babbage-002","gpt-3.5-turbo-instruct"]
-    st.sidebar.selectbox("Model seç", options = model_options, key="model")
+    model_options = ["gpt-3.5-turbo-0125", "babbage-002", "gpt-3.5-turbo-instruct"]
+    st.sidebar.selectbox("Select model", options=model_options, key="model")
 
-    st.sidebar.header("Doküman Yükle")
-    uploaded_file = st.sidebar.file_uploader("", type=["pdf","docx","txt"])
+    st.sidebar.header("Upload a document")
+    uploaded_file = st.sidebar.file_uploader("Choose a document", type=["pdf", "docx", "txt"], label_visibility="collapsed")
     if uploaded_file is not None:
-        if st.sidebar.button("Yükle"):
-            with st.spinner("Yükleniyor..."):
+        if st.sidebar.button("Upload", key="upload_button"):
+            with st.spinner("Uploading..."):
                 upload_response = upload_document(uploaded_file)
                 if upload_response:
-                    st.sidebar.success(f"Doküman {uploaded_file} başarıyla yüklendi!")
+                    st.sidebar.success(f"File {uploaded_file.name} uploaded successfully!")
                     st.session_state.documents = list_documents()
 
-    st.sidebar.header("Yüklenen Dokümanlar")
-    if st.sidebar.button("Yenile"):
-        with st.spinner("Dokümanlar yeniden yükleniyor..."):
+    st.sidebar.header("Uploaded documents")
+    if st.sidebar.button("Refresh", key="refresh_button"):
+        with st.spinner("Refreshing uploaded documents..."):
             st.session_state.documents = list_documents()
+
+    if "documents" not in st.session_state:
+        st.session_state.documents = list_documents()
+
+    documents = st.session_state.documents
+    if documents:
+        for doc in documents:
+            st.sidebar.text(doc["filename"])
+
